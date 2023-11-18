107a2514f31494fb02dbed8be8e7aba8e0b8ddae
mochi-neko <t.o.e.4315@gmail.com>
2023-11-18 15:41:42 +0900

    Implement simple demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use dioxus::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    dioxus_web::launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    let list = use_state(&cx, || Vec::<String>::new());
+
+    cx.render(rsx! {
+        h1 { "ToDo List" }
+
+        input {
+            r#type: "text",
+            oninput: move |event| list.with_mut(|list| list.push(event.value.clone())),
+        }
+
+        button {
+            onclick: move |_| list.with_mut(|list| list.clear()),
+            "Clear All"
+        }
+
+        ul {
+            list.iter().map(|task| rsx!(
+                li { "{task}" }
+            ))
+        }
+    })
 }
